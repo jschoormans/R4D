bfd891bce94535032099afa8adc30ca9f418113d
jschoormans <jasper.schoormans@gmail.com>
2018-04-26 17:12:58 +0200

    gpu implementation
    just 5 times faster...

--- Examples/examples_4D.m
+++ Examples/examples_4D.m
@@ -33,7 +33,7 @@ MR.CalculateAngles;
 MR.PhaseShift;
 MR.Data=ifft(MR.Data,[],3); %eventually: remove slice oversampling
 [nx,ntviews,ny,nc]=size(MR.Data);
-goldenangle=MR.Parameter.GetValue('`EX_ACQ_radial_golden_ang_angle');
+goldenangle=MR.Parameter.GetValue('`CSC_golden_angle');
 k=buildRadTraj2D(nx,ntviews,false,true,true,[],[],[],[],goldenangle);
 
 
@@ -43,8 +43,9 @@ TR=MR.Parameter.Scan.TR;
 halfscan=MR.Parameter.Scan.HalfScanFactors(2);
 
 P.binparams.Fs=(size(MR.Data,3)*halfscan*TR*1e-3)^(-1); %!!!
-P.binparams.goldenangle=MR.Parameter.GetValue('`EX_ACQ_radial_golden_ang_angle');
-
+P.binparams.goldenangle=MR.Parameter.GetValue('`CSC_golden_angle');
+P.binparams.PCAfreqband=[0.1 0.5]
+P.binparams.sortingmethod='v'
 [kdatau,ku] = ksp2frames(MR.Data,k,P.binparams);
 
 %% DO CS
